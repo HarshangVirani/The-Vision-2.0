72c198609dfa338e1380aecbe9b5bdef760d07fb
abc <harshang.008@gmail.com>
2020-12-18 15:44:14 -0500

    Sign up and login screen successfull...

---
+++ app/src/main/java/com/example/thevision2/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.thevision2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView logo;
+    private TextView namePlate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setReference();
+        setSplashScreen();
+        changeStatusBarColor();
+    }
+    private void setReference() {
+        logo = findViewById(R.id.logo);
+        namePlate = findViewById(R.id.text_view);
+    }
+
+    private void setSplashScreen(){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, SignUpAndLoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+
+    private void changeStatusBarColor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(getResources().getColor(R.color.white));
+        }
+    }
+
+}
