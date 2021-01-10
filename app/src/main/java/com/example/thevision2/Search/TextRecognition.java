b40e22799eca59d6ec87843ae09618e79a179032
abc <harshang.008@gmail.com>
2021-01-10 05:49:22 -0500

    Text Recognition is done.

---
+++ app/src/main/java/com/example/thevision2/Search/TextRecognition.java
@@ -0,0 +1,171 @@
+package com.example.thevision2.Search;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.example.thevision2.R;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.IOException;
+import java.util.Locale;
+
+public class TextRecognition extends AppCompatActivity {
+
+    private SurfaceView cameraView;
+    private TextView textView;
+    private CameraSource cameraSource;
+    private final int RequestCameraPermissionID = 1000;
+    private String tag = "TextRecognition";
+    private TextToSpeech tts;
+    private String stringText;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case RequestCameraPermissionID:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    try {
+                        cameraSource.start(cameraView.getHolder());
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_recognition);
+
+        setReference();
+        setTextRecognition();
+    }
+
+    private void setReference() {
+        cameraView = findViewById(R.id.surface_view);
+        textView = findViewById(R.id.text_msg);
+    }
+
+    private void setTextRecognition() {
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+        if (!textRecognizer.isOperational()) {
+            Log.d(tag, "Detector dependencies are not yet available");
+        } else {
+            cameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer)
+                    .setFacing(CameraSource.CAMERA_FACING_BACK)
+                    .setRequestedFps(2.0f)
+                    .setAutoFocusEnabled(true)
+                    .build();
+            cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+                @Override
+                public void surfaceCreated(SurfaceHolder holder) {
+                    try {
+                        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                            ActivityCompat.requestPermissions(TextRecognition.this,
+                                    new String[]{Manifest.permission.CAMERA},RequestCameraPermissionID);
+                            return;
+                        }
+                        cameraSource.start(cameraView.getHolder());
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+                }
+
+                @Override
+                public void surfaceDestroyed(SurfaceHolder holder) {
+                 cameraSource.stop();
+                }
+            });
+
+            textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
+                @Override
+                public void release() {
+
+                }
+
+                @Override
+                public void receiveDetections(Detector.Detections<TextBlock> detections) {
+                    final SparseArray<TextBlock> item = detections.getDetectedItems();
+                    if (item.size() != 0){
+                        textView.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                StringBuilder stringBuilder = new StringBuilder();
+                                for (int i=0; i<item.size();++i){
+                                    TextBlock textBlock = item.valueAt(i);
+                                    stringBuilder.append(textBlock.getValue());
+                                    stringBuilder.append("\n");
+                                }
+                                textView.setText(stringBuilder.toString());
+                                stringText = textView.getText().toString();
+                                textToSpeech();
+                            }
+                        });
+                    }
+                }
+            });
+        }
+    }
+
+    private void textToSpeech() {
+        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    tts.setLanguage(Locale.UK);
+                }
+            }
+        });
+        final Handler h = new Handler();
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                tts.setSpeechRate(0.8f);
+                tts.setPitch(1.0f);
+                tts.speak(stringText, TextToSpeech.QUEUE_FLUSH, null, null);
+
+            }
+        }, 500);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        tts.stop();
+        stringText.equals(null);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        tts.shutdown();
+    }
+}
